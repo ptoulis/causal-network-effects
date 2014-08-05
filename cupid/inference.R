e35a72e893b9fd8d86d36f3375809539b2770e78
ptoulis <ptoulis@fas.harvard.edu>
2014-08-04 23:40:19 -0400

    Full MH implementation for the posterior CACE. It works.

--- cupid/inference.R
+++ cupid/inference.R
@@ -5,6 +5,8 @@
 # par = parameters (df)
 # mu.male, sigma.male, mu.female, sigma.female
 #
+# data = LIST(Yt, Yc, tc.true) -- same length.
+#        tc.true = vector of true types.
 source("population.R")
 CHECK_par <- function(par) {
   CHECK_identical(names(par), c("mu.male", "sigma.male", 
@@ -13,11 +15,13 @@ CHECK_par <- function(par) {
 
 get.Y.types <- function(data, tc.imp) {
   # Will read the imputed vector and return 
-  # a LIST(y.males, y.females) of the observed outcomes.
+  # a LIST(male, female) of the observed outcomes.
+  #
+  # f(Yt, Yc, C) = (male=..., female=...)
   males = which(tc.imp=="M")
   females = which(tc.imp=="F")
   CHECK_MEMBER(tc.imp, c("M", "F"))
-  # CHECK_TRUE(length(males)==length(females), msg="#m= #f")
+
   CHECK_TRUE(length(tc.imp)==length(data$Yc), msg="correct #imputations")
   
   return(list(male = c(data$Yt, data$Yc[males]),
@@ -51,20 +55,155 @@ sample.par <- function(data, tc.imp, par.old) {
   return(par.new)
 }
 
+complete.data.log.likelihood <- function(data, tc.imp, par) {
+  # Given some model parameters "par" and data and an inputed type vector
+  # compute the log-likelihood.
+  #
+  Y = get.Y.types(data, tc.imp)
+  log.lik.male = sum(dnorm(Y$male, mean = par$mu.male, sd = par$sigma.male, log=T))
+  log.lik.female = sum(dnorm(Y$female, mean=par$mu.female, sd = par$sigma.female, log=T))
+  
+  return(log.lik.male + log.lik.female)
+}
+
 complete.data.estimate <- function(data, tc.imp) {
+  # Given an imputed type vector, compute the estimand.
+  #
   Y = get.Y.types(data, tc.imp) # do this to check tc.imp
   males = which(tc.imp=="M")
   return(mean(data$Yt) - mean(data$Yc[males]))
 }
 
 init.par <- function(data) {
+  # Initialize the model parameter.
   return(list(mu.male = mean(data$Yt), sigma.male=sd(data$Yt),
               mu.female = mean(data$Yc), sigma.female=sd(data$Yc)))
 }
 
 random.types <- function(Nc) {
+  CHECK_TRUE(Nc %% 2==0, msg="even #units in the Control group")
   sample(c(rep("M", Nc/2), rep("F", Nc/2)))
 }
+
+perturb.types <- function(tc.imp, depth=2) {
+  if(depth==0) return(tc.imp)
+  m = sample(which(tc.imp=="M"), size=1)
+  f = sample(which(tc.imp=="F"), size=1)
+  # exchange between one male-female
+  tc.imp[m] <- "F"
+  tc.imp[f] <- "M"
+  return(perturb.types(tc.imp, depth-1))
+}
+
+generate.data <- function(Nt, Nc) {
+  # Generates a data object
+  #
+  mu.m = 3.5  # male mean
+  mu.f = 5.5  # female mean
+  se.m = 1.5
+  se.f = 2.5
+  
+  par(mfrow=c(1, 1))
+  # Sample male outcomes.
+  Yt = rnorm(Nt, mean=mu.m, sd=se.m)
+  Yc = rnorm(Nc, mean=mu.m, sd=se.m)
+  # Sample female outcomes.
+  tc.true = random.types(Nc)
+  
+  females = which(tc.true=="F")
+  Yc[females] = rnorm(length(females), mean=mu.f, sd=se.f)
+
+  return(list(Yt=Yt, Yc=Yc, tc.true=tc.true))
+}
+
+#### TESTING.
+test.likelihood <- function() {
+  # Shows the likelihood ratios around the true value.
+  d = generate.data(300, 400)
+  tc =  d$tc.true # true types.
+  p = init.par(d)
+  L0 = complete.data.log.likelihood(d, tc, p)
+  print(sprintf("True-data likelihood = %.3f", L0))
+  
+  mh.ratio <- function(ll.values) {
+    # need to show min(1, exp(ll-L0))  values
+    sapply(ll.values, function(i) min(1, exp(i - L0)))
+  }
+  
+  x = replicate(100, { complete.data.log.likelihood(d, perturb.types(tc, 1), p)})
+  par(mfrow=c(2, 2))
+  hist(mh.ratio(x), main="Histogram logL(perturb=1)", xlim=c(0, 1))
+  x = replicate(100, { complete.data.log.likelihood(d, perturb.types(tc, 2), p) })
+  hist(mh.ratio(x), main="Histogram logL(perturb=2)", xlim=c(0, 1))
+  x = replicate(100, { complete.data.log.likelihood(d, perturb.types(tc, 5), p) })
+  hist(mh.ratio(x), main="Histogram logL(perturb=5)", xlim=c(0, 1))
+  x = replicate(100, { complete.data.log.likelihood(d, random.types(400), p) })
+  hist(mh.ratio(x), main="Histogram logL(random)", xlim=c(0, 1))
+}
+
+## Inference methods start here.
+#
+# Simple MH
+#
+correct.types.ratio <- function(data, tc.imp) {
+  # Computes the ratio of correct types in the imputed vector.
+  #
+  length(which(tc.imp==data$tc.true)) / length(data$Yc)
+}
+
+mcmc.mh <- function(data, niters=1000, verbose=T) {
+  
+  K = length(data$Yc)
+  # Initialization
+  types = random.types(K)  # holds the imputed vector of types.
+  types.matrix = matrix(NA, nrow=niters, ncol=K)
+  nacc = 0  # acceptance.
+  par = init.par(data)
+  est = c()
+  plot.times = as.integer(seq(10, niters, 
+                              length.out=max(1, as.integer(niters/1000))))
+  
+  true.est = complete.data.estimate(data, data$tc.true)
+  print(sprintf("True complete-data estimate = %.3f", true.est))
+  for(iter in 1:niters) {
+    new.types <- perturb.types(types, depth=1)
+    
+    new.ll = complete.data.log.likelihood(d, new.types, par)
+    old.ll = complete.data.log.likelihood(d, types, par)
+    if(verbose) {
+      print(sprintf("OLD: ll=%.2f CR=%.1f%% -- NEW: ll=%.2f CR=%.1f%% -- diff=%.2f ll.ratio=%.2f",
+                    old.ll, 100 * correct.types.ratio(data, types),
+                    new.ll, 100 * correct.types.ratio(data, new.types),
+                    new.ll - old.ll,
+                    exp(new.ll - old.ll)))
+    }
+    if(iter %in% plot.times) {
+      plot(est, type="l", ylim=c(true.est-1, true.est+1))
+      abline(h=true.est, col="red")
+    }
+    log.acc = min(0, new.ll - old.ll)
+    if(runif(1) < exp(log.acc)) {
+      nacc <- nacc + 1
+      types <- new.types
+    }
+    # 1b. update the types matrix
+    types.matrix[iter, ] <- types=="M"
+    
+    # 2. Sample parameter
+    par = sample.par(data, tc.imp = types, par.old = par)
+    
+    # new estimate
+    est = c(est, complete.data.estimate(data, types))
+  }
+  print("Chain information")
+  avg.types = as.numeric(colMeans(types.matrix) > 0.5) + 1
+  avg.types = c("F", "M")[avg.types]
+  # print(table(avg.types))
+  print(sprintf("Correct types in chain %.1f%%", 100 * correct.types.ratio(data, avg.types)))
+  print(sprintf("MH acceptance ratio %.2f%%", 100 * nacc/ niters))
+  return(tail(est, 0.2 * length(est)))
+}
+
 
 new.statistic.mcmc <- function(data, niters=1000) {
   Yt = data$Yt
@@ -98,52 +237,6 @@ new.statistic.mcmc <- function(data, niters=1000) {
   print(table(avg.types))
   print(sprintf("Correct %.1f%%", 100 * length(which(avg.types==data$tc.true)) / K))
   return(tail(est, 0.2 * length(est)))
-}
-
-
-generate.dataset <- function(Nt, Nc) {
-  mu.m = 3.5
-  mu.f = 10.5
-  se.m = 1.5
-  se.f = 2.5
-  
-  par(mfrow=c(1, 1))
-  
-  Yt = rnorm(Nt, mean=mu.m, sd=se.m)
-  Yc = rnorm(Nc, mean=mu.m, sd=se.m)
-  
-  tc.true = random.types(Nc)
-  
-  females = which(tc.true=="F")
-  Yc[females] = rnorm(length(females), mean=mu.f, sd=se.f)
-  return(list(Yt=Yt, Yc=Yc, tc.true=tc.true))
-}
-
-test.mcmc <- function(niters.mcmc=1000) {
-  D = generate.dataset(Nt=250, Nc=500)
-  p2 <- hist(D$Yc, freq=F, breaks=20, col=rgb(0, 0, 0.5, 0.1))      
-  p1 <- hist(D$Yt, freq=F, breaks=20, col=rgb(1,0, 0, 0.2), add=T)   
-  
-  print(sprintf("True estimate %.3f", 
-                complete.data.estimate(D, tc.imp = D$tc.true)))
-  est = new.statistic.mcmc(D,  niters=niters.mcmc)
-  print(summary(est))
-  plot.new()
-  frame()
-  m = mcmc(est)
-  plot(m)
-  return(m)
-}
-
-
-test.statistic.nonparametric <- function(Yt, Yc) {
-  y1 = mean(Yt)
-  ordered.control.units = order(abs(Yc-y1))
-  
-}
-
-cupid.effects <- function(pop) {
-  
 }
 
 
