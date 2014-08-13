19fe00ac2be6a6ef1927207eaacdbf5a80ab9792
Panos Toulis <ptoulis@fas.harvard.edu>
2014-08-12 22:12:29 -0400

    Experiments with the alternative hypothesis

--- cupid/main-odyssey.R
+++ cupid/main-odyssey.R
@@ -1,7 +1,7 @@
 source("inference.R")
 # First argument determines how many iterations(imputations) to perform for ech p-value
 args <- commandArgs(trailingOnly = TRUE)
-data = generate.data(Nt=200, Nc=300, cupid.effect=0);
+data = generate.data(Nt=200, Nc=300, cupid.effect=0.3);
 p = NA
 if(length(args)==0) {
   p = run.most.powerful.test(data, niters=10);
