2c3b2f791e68e03864e3946e2825cf4ba73bb55a
Panos Toulis <ptoulis@fas.harvard.edu>
2014-09-26 12:14:29 -0400

    Added the option to define the true cupid effect

--- cupid/main-odyssey.R
+++ cupid/main-odyssey.R
@@ -1,11 +1,17 @@
 source("inference.R")
 # First argument determines how many iterations(imputations) to perform for ech p-value
 args <- commandArgs(trailingOnly = TRUE)
-data = generate.data(Nt=200, Nc=300, cupid.effect=0.3);
+true.effect = as.numeric(args[1])
+niters = 10
+if(length(args) == 2) {
+ niters = as.numeric(args[2])
+}
+
+data = generate.data(Nt=200, Nc=300, cupid.effect=true.effect);
 p = NA
 if(length(args)==0) {
   p = run.most.powerful.test(data, niters=10);
 } else {
-  p = run.most.powerful.test(data, niters=as.numeric(args[1]))
+  p = run.most.powerful.test(data, niters=niters)
 }
 print(p)
